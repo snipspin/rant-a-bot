be9787edc51f13d122035e6ab653834e0fe8897a
snipspin <photonenimpuls@gmail.com>
2020-02-26 19:35:53 -0800

    chat scrolls automatically

--- static/js/chat.js
+++ static/js/chat.js
@@ -3,7 +3,7 @@
     const ws = new WebSocket(HOST);
     
     const form = document.querySelector('.form');
-  
+    const messageList = document.querySelector('.msgcontent');
     form.onsubmit = function() {
       let input = document.querySelector('.input'); 
       let text = input.value;
@@ -11,6 +11,7 @@
       ws.send(text);
       input.value = '';
       input.focus();
+      messageList.scrollTop = messageList.scrollHeight;
       return false;
     }
     
@@ -21,12 +22,13 @@
     }
 
     addMessageToView = function(text, isServer = false) {
-      let messageList = document.querySelector('.msgcontent');
+      
       let div = document.createElement('div');
       // div.className = "bubble right-align";
       div.className = (isServer)? 'speech-bubble left left-align col s7' : 'speech-bubble right right-align col s7 offset-s5';
       div.textContent = text;
       console.log(div);
       messageList.appendChild(div);
+      messageList.scrollTop = messageList.scrollHeight;
     }
   }());
